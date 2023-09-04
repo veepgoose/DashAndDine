f59b5c2bce8458c6078ed3b8eb52b7670371b0e9
Tom Webb <tomwebb1992@gmail.com>
2023-09-04 16:53:42 +0100

    added a drop down for workout types, ensure Select cannot be chosen. Also tried making the sort order alphabetic

--- Screens/AddWorkout/AddWorkout.js
+++ Screens/AddWorkout/AddWorkout.js
@@ -1,23 +1,41 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { Picker } from "react-native-web";
+
+// Define the workoutTypes array with options
+const workoutTypes = ["Select", "Running", "Walking", "Yoga", "Jiu-Jitsu"];
+
+const workoutTypesAbc = workoutTypes.slice(1).sort();
+// created a variable to ensure that workout types are automatically sorted alphabetically
+workoutTypesAbc.unshift("Select");
+// still wanted Select to be on the top of the pile
 
 function AddWorkout({ navigation }) {
-  const [workoutType, setWorkoutType] = useState("");
+  const [workoutType, setWorkoutType] = useState("Select");
   const [duration, setDuration] = useState("");
   const [date, setDate] = useState("");
 
   const handleAddWorkout = () => {
-    navigation.goBack();
+    if (workoutType !== "Select") {
+      // added an if stamtent to ensure that the user selects a workout type
+      navigation.goBack();
+    } else {
+      alert("Please select a workout type");
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text>Add Workout</Text>
-      <TextInput
-        placeholder="Workout Type"
-        value={workoutType}
-        onChangeText={setWorkoutType}
-      />
+      <Picker
+        selectedValue={workoutType}
+        onValueChange={(itemValue, itemIndex) => setWorkoutType(itemValue)}
+      >
+        {workoutTypes.map((type, index) => (
+          <Picker.Item key={index} label={type} value={type} />
+        ))}
+      </Picker>
+
       <TextInput
         placeholder="Duration"
         value={duration}
